fe578482f0df8c9e0eaaeefd5fbdbc4ad95af5fe
Januda Bethmin <desilvabethmin@gmail.com>
2024-03-25 12:59:50 +0530

    last commit

--- buyer/src/main/java/io/januda/buyer/InventoryApplication.kt
+++ buyer/src/main/java/io/januda/buyer/InventoryApplication.kt
@@ -6,10 +6,13 @@ import io.januda.buyer.data.AppDataContainer
 
 class InventoryApplication : Application() {
 
+    // We create a AppContainer instance
     private lateinit var  container: AppContainer
 
     override fun onCreate() {
         super.onCreate()
+        // A App data container will be assigned here
+        // context -> The place we are calling our application
         container = AppDataContainer(this)
     }
 
--- buyer/src/main/java/io/januda/buyer/data/AppContainer.kt
+++ buyer/src/main/java/io/januda/buyer/data/AppContainer.kt
@@ -1,13 +1,17 @@
 package io.januda.buyer.data
 
 import android.content.Context
-
+// This is like the initiation point of our application
 interface AppContainer {
+    // We are telling we require a Item repository.
     val itemsRepository: ItemsRepository
 }
 
+// The implementation of above AppContainer interface.
 class AppDataContainer(private val context: Context) : AppContainer {
     override val itemsRepository: ItemsRepository by lazy {
-        OfflineItemsRepository(InventoryDatabase.getDatabase(context)!!.itemDao())
+        // Inside OfflineItemRepository we instantiate the  ItemRepository
+        val dao =  InventoryDatabase.getDatabase(context)!!.itemDao()
+        OfflineItemsRepository(dao)
     }
 }
